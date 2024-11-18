640611e559ed451f445d34c07076e2c791e59a24
Vishnu2350 <122674957+Vishnu2350@users.noreply.github.com>
2024-11-18 13:01:55 +0530

    code fix

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 
-export default function Input({label,type,placeholder,customcss}) {
+export default function Input({ label, type, placeholder, customcss }) {
   return (
-
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">{label}</span>
-        </label>
-        <input
-          type={type}
-          placeholder={placeholder}
-          className={` ${customcss} input input-bordered  `}
-          required
-        />
-      </div>
-  
-  
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">{label}</span>
+      </label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={` ${customcss} input input-bordered  `}
+        required
+      />
+    </div>
   );
 }

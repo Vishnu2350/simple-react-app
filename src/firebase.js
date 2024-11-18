e3f6ba008c403758bd737d1f004beb439a8f968c
Vishnu2350 <122674957+Vishnu2350@users.noreply.github.com>
2024-11-18 12:18:26 +0530

    initial commit of application

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+
+
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDB10H_W9DNULgRQoq3tvb_FUJ_dtun_Ho",
+  authDomain: "evnet-29695.firebaseapp.com",
+  projectId: "evnet-29695",
+  storageBucket: "evnet-29695.firebasestorage.app",
+  messagingSenderId: "483152378344",
+  appId: "1:483152378344:web:454fba083a666a992fcc5d",
+  measurementId: "G-WQW2BL19V1"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
